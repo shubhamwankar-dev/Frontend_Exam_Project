3b9d02fb418fee65feb988c56528dddd7f5dedab
Shubham Wankar <shubham.wankar.dev@gmail.com>
2025-12-25 16:12:19 +0530

    Initial Commit

--- src/components/FeatureCard/FeatureCard.jsx
+++ src/components/FeatureCard/FeatureCard.jsx
@@ -5,7 +5,7 @@ import Row from "react-bootstrap/Row";
 
 const FeatureCard = ({ icon, title, description }) => {
   return (
-    <Card className="d-flex flex-cloumn align-items-center text-center border-0">
+    <Card className="d-flex flex-cloumn align-items-center text-center border-0 p-4">
       <div className="bg-success rounded-circle h-50 px-4 py-4">{icon}</div>
 
       <Card.Body>
