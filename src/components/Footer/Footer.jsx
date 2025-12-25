939ad2e13ec9d0745d79aaf12fe6b4da44171477
Shubham Wankar <shubham.wankar.dev@gmail.com>
2025-12-25 16:14:29 +0530

    Initial Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       id="footer"
       className="d-flex justify-content-between align-items-center py-4 px-5 bg-primary flex-wrap flex-md-nowrap gap-3"
     >
-      <h3 className="m-0 p-0">LOGO</h3>
+      <h3 className="m-0 p-0 fw-bolder text-white">CodeBizz</h3>
       <div className="d-flex gap-4 fs-5">
         <a
           className=" text-white fs-6 fw-semibold text-decoration-none"
